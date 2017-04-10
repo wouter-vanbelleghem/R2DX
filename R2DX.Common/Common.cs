f48c5fe53e98982e037483e39f2746202d6c32c9
Wouter Vanbelleghem <wouter.vanbelleghem@gmail.com>
2017-04-10 18:04:23 +0200

    First Commit - Solution Structure

---
+++ R2DX.Common/Common.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R2DX.Common
+{
+    public enum AIState
+    {
+        Unknown = -1,
+        Stopped = 0,
+        Starting = 1,
+        Started = 2,
+        Stopping = 3,
+        Listening = 4,
+        Acting = 5,
+        Searching = 6,
+        Learning = 7,
+        Error = 8,
+        Question = 9
+    };
+
+    public class Functions
+    {
+        public AIState getAIState()
+        {
+            AIState tmp = AIState.Unknown;
+
+            return tmp;
+        }
+
+        public void log()
+        {
+            //log internally on mysql database (so compatible with Rasberry and linux)
+            //-- when log server (server) is reachable, synchronize?
+            //log service syncronizator must be seperate service?
+        }
+    }       
+}
---
+++ R2DX.Control.Windows/Main.cs
@@ -0,0 +1,48 @@
+﻿using R2DX.Common;
+using System;
+using System.Windows.Forms;
+
+namespace R2DX.Control.Windows
+{
+    public partial class Main : Form
+    {        
+        private AIState _AIStatus;
+        private Common.Functions _AIFunctions =  new Common.Functions();
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        ///     What do I have to do on Form load
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Start();
+        }
+
+        /// <summary>
+        ///     Initialises everything
+        /// </summary>
+        private void Start()
+        {
+            _AIStatus = _AIFunctions.getAIState();
+        }
+
+        /// <summary>
+        ///     Returns state of AI Service
+        /// </summary>
+        /// <returns>true=AI service is running</returns>
+        private bool IsAIRunning()
+        {
+            bool tmp = false;
+            
+            
+                        
+            return tmp;
+        }
+   }
+}
